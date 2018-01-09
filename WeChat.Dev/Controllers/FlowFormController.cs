5f9c615840bb18af185814e1abe63b69d9313060
蒋勇 <jiangy@highzap.com>
2018-01-09 18:22:42 +0800

    添加List 和Nav 分部视图 添加NavModel 菜单模型 重构流程表单详情界面js代码 添加移动审批2.0相关界面

---
+++ WeChat.Dev/Controllers/FlowFormController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeChat.Dev.Controllers
+{
+    /// <summary>
+    /// 流程表单
+    /// </summary>
+    public class FlowFormController : Controller
+    {
+        /// <summary>
+        /// 流程表单详情
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult FormDetail()
+        {
+            return View();
+        }
+    }
+}
--- WeChat.Dev/Models/NavModel.cs
+++ WeChat.Dev/Models/NavModel.cs
@@ -1,7 +1,53 @@
-﻿namespace WeChat.Dev.Models
+﻿using System.Collections.Generic;
+
+namespace WeChat.Dev.Models
 {
     public class NavModel
     {
+        public List<string> ClassList { get; set; }
 
+        public List<Menu> Menus { get; set; }
+    }
+
+    public class Menu
+    {
+        public string Name { get; set; }
+
+        public bool IsDisplay { get; set; }
+
+        public ButtonType ButtonType { get; set; }
+
+        /// <summary>
+        /// 代码片段
+        /// </summary>
+        public string CodeSnippet { get; set; }
+
+        /// <summary>
+        /// 跳转URL
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// 样式类集合
+        /// </summary>
+        public List<string> ClassList { get; set; }
+
+        /// <summary>
+        /// 额外的属性 Notation提示数量
+        /// </summary>
+        public dynamic Extra { get; set; }
+    }
+
+    public enum ButtonType
+    {
+        /// <summary>
+        /// 按钮
+        /// </summary>
+        Button = 0,
+
+        /// <summary>
+        /// a标签
+        /// </summary>
+        Link = 1
     }
 }
