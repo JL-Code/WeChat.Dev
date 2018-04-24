110be391bc324596e3107b5d8e292f64388969a0
蒋勇 <jiangy@highzap.com>
2018-04-24 10:08:19 +0800

    添加时间戳描述信息

--- WeChat.Core/Extensions/DateTimeExtend.cs
+++ WeChat.Core/Extensions/DateTimeExtend.cs
@@ -4,20 +4,24 @@ namespace Zap.WeChat.SDK.Extensions
 {
     public static class DateTimeExtend
     {
+
         static readonly DateTime _startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1)); // 当地时区
+
         /// <summary>
         /// C# DateTime转换为Unix时间戳
+        /// Unix时间戳(Unix timestamp)，或称Unix时间(Unix time)、POSIX时间(POSIX time)，是一种时间表示方式，定义为从格林威治时间1970年01月01日00时00分00秒(北京时间1970年01月01日08时00分00秒)起至现在的总秒数
         /// </summary>
         /// <param name="date"></param>
         /// <returns></returns>
         public static long ToUnix(this DateTime date)
         {
-            long timeStamp = (long)(DateTime.Now - _startTime).TotalSeconds; // 相差秒数
+            long timeStamp = (long)(date - _startTime).TotalSeconds; // 相差秒数
             return timeStamp;
         }
 
         /// <summary>
         ///  Unix时间戳转换为C# DateTime
+        ///  Unix时间戳(Unix timestamp)，或称Unix时间(Unix time)、POSIX时间(POSIX time)，是一种时间表示方式，定义为从格林威治时间1970年01月01日00时00分00秒(北京时间1970年01月01日08时00分00秒)起至现在的总秒数
         /// </summary>
         /// <param name="unix"></param>
         /// <returns></returns>
@@ -29,6 +33,7 @@ namespace Zap.WeChat.SDK.Extensions
 
         /// <summary>
         /// C#时间转为JS时间戳
+        /// JavaScript时间戳：是指格林威治时间1970年01月01日00时00分00秒(北京时间1970年01月01日08时00分00秒)起至现在的总毫秒数。
         /// </summary>
         /// <param name="datetime"></param>
         /// <returns></returns>
